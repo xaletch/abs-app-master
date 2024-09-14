f92122a2f2f68634088c86d752a40b8682e5f7d8
Xaletch <kiril.kolesnikov0609@mail.ru>
2024-09-14 22:58:18 +0300

    private router

--- src/domians/settings/index.tsx
+++ src/domians/settings/index.tsx
@@ -3,27 +3,13 @@ import { UpdateProfile } from "@/components/molecules/update-profile";
 import { USER_ID } from "@/constants/user";
 
 export const SettingPage = () => {
-    // const storage = localStorage.getItem(USER_ID);
-    // const userId = storage ? (JSON.parse(storage) as number) : null;
+    const storage = localStorage.getItem(USER_ID);
+    const userId = storage ? (JSON.parse(storage) as number) : null;
 
-    // const { data, isSuccess } = useGetProfileQuery(userId as number);
+    const { data, isSuccess } = useGetProfileQuery(userId as number);
 
-    // if (isSuccess && data.access == "allow") {
-    //     return <UpdateProfile user={data} />;
-    // }
-    const user = {
-        user_id: 12345,
-        access: "allow",
-        name: "Kirill",
-        surname: "Ivanov",
-        second_name: "Sergeevich",
-        city: "Moscow",
-        tg: "@kirillivanov",
-        email: "kirill.ivanov@example.com",
-        alert: "No new alerts",
-        role: "admin"
-      };
-      
-    return <UpdateProfile user={user} />;
-    // return <></>;
+    if (isSuccess && data.access == "allow") {
+        return <UpdateProfile user={data} />;
+    }
+    return <></>;
 };
